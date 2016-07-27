bffd8d0ab004aec7ac64a4801cf15072fdbb24ce
childe <heychilde@gmail.com>
2016-07-27 15:55:30 +0800

    hdr for top aggr

--- src/app/panels/percentiles/module.js
+++ src/app/panels/percentiles/module.js
@@ -136,7 +136,7 @@ define([
       if ($scope.panel.mode !== '-'){
         var sub_aggs = $scope.ejs.PercentilesAggregation('stats')
         .field($scope.panel.field)
-        .percents(percents);
+        .percents(percents).hdr(3);
 
         if ($scope.panel.compression!==''){
           sub_aggs.compression($scope.panel.compression);
